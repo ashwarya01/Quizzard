fbfb017ec310afb9658047ccd0f7d2fd5723d65d
ashwarya01 <aishwaryabharti252@gmail.com>
2022-08-02 11:54:16 +0530

    New Update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// scroll style navbar
+
+$(window).scroll(function () {
+    $scroll = $(window).scrollTop();
+    
+    if ($scroll > 0) {
+        $('#navbar').css('background', "#212529");
+    } else {
+        $('#navbar').css('background', 'transparent');
+    }
+});
+
+// end navbar
+
